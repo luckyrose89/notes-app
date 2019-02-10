b178a9fa886618a03096ff5726ace38dfb16c553
avidrutham <divyamathur89@gmail.com>
2019-02-10 13:48:30 +0530

    create form to accept notebook title

--- src/components/dashboard/CreateBook.js
+++ src/components/dashboard/CreateBook.js
@@ -1,16 +1,37 @@
 import React from "react";
+// import axios from 'axios';
 
 class CreateBook extends React.Component {
   state = {
     title: ""
   };
+
+  handleNameChange = event => {
+    this.setState({
+      title: event.taget.value
+    });
+  };
+
+  onSubmit = event => {
+    event.preventDefault();
+  };
+
   render() {
+    let { title } = this.state;
     return (
       <div>
         <p>This is where you create a notebook</p>
-        <form>
+        <form onSubmit={this.onSubmit}>
           <label htmlFor="notebookTitle">Notebook Title:</label>
-          <input id="bookTitle" />
+          <input
+            type="text"
+            value={title}
+            onChange={this.handleNameChange}
+            id="bookTitle"
+          />
+          <div>
+            <input type="submit" value="Save Notebook" />
+          </div>
         </form>
       </div>
     );
