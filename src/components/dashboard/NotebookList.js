99c5d0bbf9f431710cc66dcde4f0cb729b2883ec
avidrutham <divyamathur89@gmail.com>
2019-02-13 13:58:18 +0530

    delete feature added to notebook

--- src/components/dashboard/NotebookList.js
+++ src/components/dashboard/NotebookList.js
@@ -1,18 +1,13 @@
 import React from "react";
-import axios from "axios";
 
 class NotebookList extends React.Component {
-  handleDelete = () => {
-    axios
-      .get("http://localhost:3001/notebook/delete/" + this.props.obj._id)
-      .then(console.log("Entry Deleted"))
-      .catch(err => console.log(err));
-  };
   render() {
     return (
       <li>
         <div>{this.props.obj.title}</div>
-        <button onClick={this.handleDelete()}>Delete</button>
+        <button onClick={() => this.props.delete(this.props.obj._id)}>
+          Delete
+        </button>
         <button>Edit</button>
         <button>Add Page</button>
         <button>View</button>
--- src/components/dashboard/ViewBook.js
+++ src/components/dashboard/ViewBook.js
@@ -24,9 +24,23 @@ class ViewBook extends React.Component {
 
   displayNotebooks() {
     return this.state.notebooks.map((book, idx) => {
-      return <NotebookList obj={book} key={idx} />;
+      return <NotebookList delete={this.handleDelete} obj={book} key={idx} />;
     });
   }
+
+  handleDelete = val => {
+    let notebooksCopy = this.state.notebooks.filter(
+      notebook => notebook._id !== val
+    );
+    axios
+      .get("http://localhost:3001/notebook/delete/" + val)
+      .then(console.log("Entry Deleted"))
+      .catch(err => console.log(err));
+
+    this.setState({
+      notebooks: notebooksCopy
+    });
+  };
 
   render() {
     return (
