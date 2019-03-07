20cbd59bb958a1dda44a75c92c336cfb6168eba8
avidrutham <divyamathur89@gmail.com>
2019-03-07 14:31:22 +0530

    add get notebooks and create notebook to context

--- src/AppContext.js
+++ src/AppContext.js
@@ -4,6 +4,48 @@ import axios from "axios";
 const notebookAxios = axios.create();
 
 class AppContextProvider extends React.Component {
+  state = {
+    notebooks: []
+  };
+
+  componentDidMount() {
+    this.getNotebooks();
+  }
+
+  getNotebooks = () => {
+    return notebookAxios.get("/notebook").then(response => {
+      this.setState({ notebooks: response.data });
+      return response;
+    });
+  };
+
+  createNotebook = newNotebook => {
+    return notebookAxios.post("/notebook", newNotebook).then(response => {
+      this.setState(prevState => {
+        return { notebooks: [...prevState.notebooks, response.data] };
+      });
+      return response;
+    });
+  };
+
+  getOneNotebook = bookId => {
+    return notebookAxios.get(`/notebook/${bookId}`).then(response => {
+      return response.data;
+    });
+  };
+
+  //   editOneNotebook = (bookId, bookObj) => {
+  //     return notebookAxios
+  //       .put(`/notebook/${bookId}`, bookObj)
+  //       .then(response => {});
+  //   };
+  deleteOneNotebook = () => {};
+
+  createNotepage = () => {};
+  getOneNotepage = () => {};
+  editOneNotepage = () => {};
+  deleteOneNotepage = () => {};
+
   render() {
     return <div>This is where we provide a consumer</div>;
   }
